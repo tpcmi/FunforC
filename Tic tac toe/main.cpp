c98e0785c24ac4878406d9f8d85ecbe523f6a2b8
tpcmi <724801800@qq.com>
2020-08-15 23:38:07 +0800

    Tic tac toe-200815

--- Tic tac toe/main.cpp
+++ Tic tac toe/main.cpp
@@ -2,13 +2,52 @@
 
 using namespace std;
 
-char square[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char square[10] = {'_','1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+void board();
+int checkwin(int &);
 
 int main(){
-    
-}
+	char label;
+	cout << "Start Game? (y/n)";
+	cin >> label;
+	while(label=='y'){
+		int player = 1,flag,choice,count = 0;
+		char mark;
+		do{
+			board();
+			player = (player % 2) ? 1 : 2;
+			cout << "Player " << player << ", enter a number:   ";
+			cin >> choice;
+			count++;
+			mark = (player == 1) ? 'X' : 'O';
+			if (choice==square[choice]-'0')
+				square[choice] = mark;
+			else{
+				cout << "Invalid move";
+				player--;
+				cin.ignore();
+				cin.get();
+				count--;
+			}
+			flag = checkwin(count);
+			player++;
+		} while(flag==-1);
+		board();
+		if(flag==1)
+			cout << "==> Player " << --player << " win!\n";
+		else
+			cout << "Game draw!\n" << endl;
 
-int checkwin(){
+		cout << "Restart Game? (y/n)";
+		cin >> label;
+	}
+	return 0;
+}
+	
+
+// 检查对局情况；
+int checkwin(int &count){
     if (square[1] == square[2] && square[2] == square[3])
 		return 1;
 	else if (square[4] == square[5] && square[5] == square[6])
@@ -25,9 +64,25 @@ int checkwin(){
 		return 1;
 	else if (square[3] == square[5] && square[5] == square[7])
 		return 1;
+	else if (count==9)
+		return 0;
+	else
+		return -1;
+	}
 
-}
-
+// 更改屏幕值；
 void board(){
-
+	system("cls");
+	cout << "=== Tic tac toe ===" << endl;
+	cout << "Player 1: 'X' , Player 2: 'O' " << endl;
+	cout << endl;
+	cout << "   " << "  |     |  " <<  endl;
+	cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
+	cout << "_____|_____|_____" <<  endl;
+	cout << "     |     |  " << endl;
+	cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |  " << endl;
+	cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
+	cout << "     |     |     " << endl;
 }
