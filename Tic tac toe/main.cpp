1bcbcdd04463ed204072236867623424615d306b
tpcmi <724801800@qq.com>
2020-08-15 22:03:22 +0800

    Tic-tac-toe-200815

---
+++ Tic tac toe/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+char square[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+int main(){
+    
+}
+
+int checkwin(){
+    if (square[1] == square[2] && square[2] == square[3])
+		return 1;
+	else if (square[4] == square[5] && square[5] == square[6])
+		return 1;
+	else if (square[7] == square[8] && square[8] == square[9])
+		return 1;
+	else if (square[1] == square[4] && square[4] == square[7])
+		return 1;
+	else if (square[2] == square[5] && square[5] == square[8])
+		return 1;
+	else if (square[3] == square[6] && square[6] == square[9])
+		return 1;
+	else if (square[1] == square[5] && square[5] == square[9])
+		return 1;
+	else if (square[3] == square[5] && square[5] == square[7])
+		return 1;
+
+}
+
+void board(){
+
+}
